cf63962f8f8a1fc36365afe2260a3642f163c5e3
tristanplx <tristan.panhelleux@gmail.com>
2024-05-15 21:49:02 +0200

    création d'un serveur qui met à jour une variable int en fonction d'une valeur choisie par le client + tentative infructueuse de makefile global

--- client/client.c
+++ client/client.c
@@ -1,2 +1,48 @@
 #include "pse.h"
-#define CMD "client"
+
+#define BUFFER_SIZE 1024
+
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    fprintf(stderr, "usage: %s hostname port\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  int sock = 0;
+  struct sockaddr_in serv_addr;
+  char buffer[BUFFER_SIZE] = {0};
+  int port = atoi(argv[2]);
+
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock < 0) {
+    perror("socket failed");
+    exit(EXIT_FAILURE);
+  }
+
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(port);
+
+  if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0) {
+    perror("Invalid address/ Address not supported");
+    close(sock);
+    exit(EXIT_FAILURE);
+  }
+
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    perror("Connection Failed");
+    close(sock);
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Connecté au serveur %s sur le port %d.\n", argv[1], port);
+  printf("Entrez un entier : ");
+  while (fgets(buffer, BUFFER_SIZE, stdin) != NULL) {
+    send(sock, buffer, strlen(buffer), 0);
+    printf("Entrez un entier : ");
+    memset(buffer, 0, BUFFER_SIZE);
+  }
+
+  close(sock);
+  return 0;
+}
+
--- serveur/serveur.c
+++ serveur/serveur.c
@@ -1,3 +1,66 @@
 #include "pse.h"
+
 #define CMD "serveur"
+#define VRAI 1
+#define BUFFER_SIZE 1024
+
+int variable = 0;  // Variable à mettre à jour
+
+int main(int argc, char *argv[]) {
+  short port;
+  int ecoute, canal, ret;
+  struct sockaddr_in adrEcoute, adrClient;
+  unsigned int lgAdrClient;
+  char buffer[BUFFER_SIZE] = {0};
+
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s port\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  port = (short)atoi(argv[1]);
+
+  printf("%s: creating a socket\n", CMD);
+  ecoute = socket(AF_INET, SOCK_STREAM, 0);
+  if (ecoute < 0)
+    erreur_IO("socket");
+
+  adrEcoute.sin_family = AF_INET;
+  adrEcoute.sin_addr.s_addr = INADDR_ANY;
+  adrEcoute.sin_port = htons(port);
+  printf("%s: binding to INADDR_ANY address on port %d\n", CMD, port);
+  ret = bind(ecoute, (struct sockaddr *)&adrEcoute, sizeof(adrEcoute));
+  if (ret < 0)
+    erreur_IO("bind");
+
+  printf("%s: listening to socket\n", CMD);
+  ret = listen(ecoute, 5);
+  if (ret < 0)
+    erreur_IO("listen");
+
+  while (VRAI) {
+    printf("%s: accepting a connection\n", CMD);
+    lgAdrClient = sizeof(adrClient);
+    canal = accept(ecoute, (struct sockaddr *)&adrClient, &lgAdrClient);
+    if (canal < 0)
+      erreur_IO("accept");
+
+    printf("%s: adr %s, port %hu\n", CMD,
+           stringIP(ntohl(adrClient.sin_addr.s_addr)), ntohs(adrClient.sin_port));
+
+    while (read(canal, buffer, BUFFER_SIZE) > 0) {
+      variable = atoi(buffer);
+      printf("Variable mise à jour : %d\n", variable);
+      memset(buffer, 0, BUFFER_SIZE);
+    }
+
+    if (close(canal) == -1)
+      erreur_IO("fermeture canal");
+  }
+
+  if (close(ecoute) == -1)
+    erreur_IO("fermeture ecoute");
+
+  exit(EXIT_SUCCESS);
+}
 
